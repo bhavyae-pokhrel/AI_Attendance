f94a0909818b08f7b925213a8cd3a400ab231233
Bhavyae <bhavyaeprasadpokhrel@gmail.com>
2026-06-12 00:51:11 +0530

    update the url

--- src/components/dialog_share_subject.py
+++ src/components/dialog_share_subject.py
@@ -7,7 +7,7 @@ import io #! when handle binary data, io module provides tools for working with 
 
 @st.dialog("Share Class Link")
 def share_subject_dialog(subject_name, subject_code):
-    app_domain = "snapclass-main.streamlit.app"
+    app_domain = "ai-snapclasses-main.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
 
     st.header("Scan to Join")
